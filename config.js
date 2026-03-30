e8c03b5808064bd1c46e7f3a3d17eaebd78057ff
Mohamed amaan <121436543+modamaan@users.noreply.github.com>
2026-03-31 01:06:46 +0530

    Update countdown date and Google Drive links

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
 window.appConfig = {
   // Set your countdown target date here. 
   // Format: "YYYY-MM-DDTHH:MM" (e.g., "2026-12-31T23:59")
-  date: "2026-04-07T15:30",
+  date: "2026-03-31T13:07",
 
   // Paste your Google Drive share links here.
   // Make sure each file is set to "Anyone with the link can view".
   // You can paste as many links as you want, one per line.
   links: `
-https://drive.google.com/file/d/1_Example-ID-Replace-Me/view?usp=sharing
+https://store.storeimages.cdn-apple.com/1/as-images.apple.com/is/MGFQ4?wid=4000&hei=4000&fmt=jpeg&qlt=90&.v=1757534575976
 https://drive.google.com/file/d/2_Example-ID-Replace-Me/view?usp=sharing
   `
 };
