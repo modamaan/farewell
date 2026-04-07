67631225eefc6458675b39207a7ccce1e0bac4be
Mohamed amaan <121436543+modamaan@users.noreply.github.com>
2026-04-07 12:49:10 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,12 +2,12 @@ window.appConfig = {
   // Set your countdown target date here. 
   // Format: "YYYY-MM-DDTHH:MM:SS+05:30" (for IST timezone)
   // Example: "2026-12-31T23:59:00+05:30"
-  date: "2026-04-07T12:42:00+05:30",
+  date: "2026-04-07T12:50:00+05:30",
 
   // Paste your Google Drive share links here.
   // Make sure each file is set to "Anyone with the link can view".
   // You can paste as many links as you want, one per line.
   links: `
-https://drive.google.com/file/d/1tKXqQ-fndfnC9zSap8brk3b4IjDSLH9G/view?usp=drivesdk
+[video] https://drive.google.com/file/d/1tKXqQ-fndfnC9zSap8brk3b4IjDSLH9G/view?usp=sharing
   `
 };
